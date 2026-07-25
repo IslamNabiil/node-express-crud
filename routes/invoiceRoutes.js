9789bf6bf6889b64949e202736af1976606a78d0
IslamNabiil <eslamoka1123@gmail.com>
2026-07-25 16:52:34 +0300

    making new route to be able to see all return inv

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 const invoiceController = require("../controller/invoiceController");
 
-router.post("/", invoiceController.createInv);
+// 1️⃣ المسارات الثابتة أولاً (Static Routes)
+router.get("/return", invoiceController.getAllReturnInv);
+router.post("/return", invoiceController.createReturnInv);
+
+// 2️⃣ المسارات العامة بدون Parameters
 router.get("/", invoiceController.getAllInv);
+router.post("/", invoiceController.createInv);
+
+// 3️⃣ المسارات المتغيرة بالـ ID دائماً في الآخر (Dynamic Routes)
 router.get("/:id", invoiceController.getInvById);
-router.delete("/:id", invoiceController.deleteInv);
 router.patch("/:id", invoiceController.updateInv);
-router.post('/return/',invoiceController.createReturnInv)
+router.delete("/:id", invoiceController.deleteInv);
 
 module.exports = router;
